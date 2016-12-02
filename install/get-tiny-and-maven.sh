672d8d5fc2e02090fcebb3fdc4bfc19c96a52e67
Joao Antonio Ferreira <joao.parana@gmail.com>
2016-12-02 09:47:45 -0200

    Acertatndo diretório install

--- install/get-tiny-and-maven.sh
+++ install/get-tiny-and-maven.sh
@@ -8,4 +8,3 @@ export TINI_VERSION=0.9.0
 curl -O -L "https://github.com/krallin/tini/releases/download/v${TINI_VERSION}/tini_${TINI_VERSION}.rpm"
 mv "tini_${TINI_VERSION}.rpm" tini-rpm/
 curl -L -O http://mirror.nbtelecom.com.br/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-mv apache-maven-3.3.9-bin.tar.gz maven3/
